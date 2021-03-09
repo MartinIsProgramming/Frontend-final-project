bfbe6d547f1b9f2ed158d38a96a92a645a121f2b
Facundo Vera <martin91.vera@gmail.com>
2021-03-09 11:04:39 -0300

    SWR and Axios

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,17 @@
 import '../scss/index.scss';
 import 'bootstrap/dist/css/bootstrap.css';
 import Navigation from '../components/navigation';
+import axios from 'axios';
+import { SWRConfig } from 'swr';
 
 function MyApp({ Component, pageProps }) {
+  const fetcher = url => axios.get(url).then(res => res.data);
   return (
     <>
       <Navigation />
-      <Component {...pageProps} />
+      <SWRConfig value={{ fetcher, revalidateOnFocus: false }}>
+        <Component {...pageProps} />\
+      </SWRConfig>
     </>
   );
 }
