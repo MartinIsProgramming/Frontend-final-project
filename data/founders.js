ba93fc324c7e8ea1b03a8c589379b239aa47baba
Facundo Vera <martin91.vera@gmail.com>
2021-03-15 10:30:36 -0300

    About page and Founders data

---
+++ data/founders.js
@@ -0,0 +1,25 @@
+const founders = [
+  {
+    image: '/images/_about/cofounder_1.jpg',
+    name: 'Juan Perez',
+    title: 'Co-Founder',
+    description:
+      'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Exercitationem, cum.',
+  },
+  {
+    image: '/images/_about/cofounder_2.jpg',
+    name: 'Maria Rodriguez',
+    title: 'Co-Founder',
+    description:
+      'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Exercitationem, cum.',
+  },
+  {
+    image: '/images/_about/cofounder_3.jpg',
+    name: 'Juan Gomez',
+    title: 'Co-Founder',
+    description:
+      'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Exercitationem, cum.',
+  },
+];
+
+export default founders;
---
+++ pages/about.js
@@ -0,0 +1,124 @@
+import Head from 'next/head';
+import { Button, Container } from 'react-bootstrap';
+import Image from 'react-bootstrap/Image';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+
+//FOUNDERS DATA
+import Data from '../data/founders';
+
+// FONTAWESOME ICONS
+import { faEnvelope } from '@fortawesome/free-regular-svg-icons';
+import {
+  faFacebook,
+  faLinkedin,
+  faTwitterSquare,
+} from '@fortawesome/free-brands-svg-icons';
+
+//BOOTSTRAP
+import { Card } from 'react-bootstrap';
+
+export default function About() {
+  const foundersInfo = Data;
+
+  const iconsInfo = [
+    {
+      icon: faFacebook,
+      path: 'https://www.facebook.com/',
+    },
+    {
+      icon: faLinkedin,
+      path: 'https://www.linkedin.com/',
+    },
+    {
+      icon: faTwitterSquare,
+      path: 'https://www.twitter.com/',
+    },
+  ];
+
+  return (
+    <>
+      <Head>
+        <title>Create Next App</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main>
+        <Container>
+          {/* STORY SECTION */}
+          <div className="title-container">
+            <h1>Our Story</h1>
+          </div>
+          <div className="divider" />
+
+          <div className="about-grid">
+            <div className="image">
+              <Image src="/images/_about/about.jpg" thumbnail fluid />
+            </div>
+            <div className="text-content">
+              <h2>CARSHOP</h2>
+              <p>
+                Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+                Consequatur, magni quasi maiores obcaecati quisquam architecto
+                totam, delectus eos ea nostrum tempora.
+              </p>
+              <p>
+                Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+                Dignissimos quia animi consequuntur distinctio corrupti
+                repudiandae corporis aliquid.
+              </p>
+              <p>
+                Lorem ipsum dolor sit, amet consectetur adipisicing elit. Labore
+                necessitatibus vero tempora, minus voluptatibus ad?
+              </p>
+              <Button variant="light" size="sm">
+                <FontAwesomeIcon
+                  style={{ width: '15px', marginRight: '5px' }}
+                  icon={faEnvelope}
+                />
+                Contact
+              </Button>
+            </div>
+          </div>
+
+          {/* FOUNDERS SECTION */}
+          <div className="title-container">
+            <h1>Meet the team</h1>
+          </div>
+          <div className="divider" />
+
+          <div className="cards-grid">
+            {foundersInfo.map((founder, index) => {
+              const { image, name, title, description } = founder;
+              return (
+                <Card key={index}>
+                  <Card.Img variant="top" src={image} />
+                  <Card.Body className="text-center">
+                    <div className="card-title">
+                      <h5 className="mb-1">{name}</h5>
+                      <span className="text-muted">{title}</span>
+                    </div>
+                    <p>{description}</p>
+
+                    <div className="social-wrapper">
+                      {iconsInfo.map((icon, index) => {
+                        return (
+                          <a key={index} href={icon.path}>
+                            <FontAwesomeIcon
+                              className="icon"
+                              style={{ width: '1.2rem', color: 'blue' }}
+                              icon={icon.icon}
+                            />
+                          </a>
+                        );
+                      })}
+                    </div>
+                  </Card.Body>
+                </Card>
+              );
+            })}
+          </div>
+        </Container>
+      </main>
+    </>
+  );
+}
