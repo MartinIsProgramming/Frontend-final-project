fb436367fd74e24b4b799d6d40f6907a3f2b5cf7
Facundo Vera <martin91.vera@gmail.com>
2021-03-16 08:59:46 -0300

    Filter functionality

--- components/filter.js
+++ components/filter.js
@@ -1,11 +1,11 @@
 import { useState, useEffect } from 'react';
 import axios from 'axios';
-import { Formik, Form } from 'formik';
 import Customlabel from '../components/customLabel';
 import CustomSelect from './customSelect';
 
 // BOOTRSTRAP STYLES
 import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
 
 const Filter = ({ setCarsInfo, inDollars, setInDollars }) => {
   const [brands, setBrands] = useState([]);
@@ -13,20 +13,30 @@ const Filter = ({ setCarsInfo, inDollars, setInDollars }) => {
   const [models, setModels] = useState([]);
   const [model, setModel] = useState('');
 
-  // GETTING MY BRANDS
-  const getBrands = () => {
+  //console.log(brands, model);
+
+  // GETTING MY INITIAL BRANDS AND MODELS
+  const getBrandsAndModelsOnLoad = () => {
     const brandsUrl = 'https://ha.edu.uy/api/brands';
+    //Firs we get all the brands
     axios.get(brandsUrl).then(res => {
       setBrands(res.data);
+      setBrand(res.data[0]);
+      //After we resolve the brands we get the models with index[0]
+      const modelsUrl = `https://ha.edu.uy/api/models?brand=${res.data[0]}`;
+      axios.get(modelsUrl).then(res2 => {
+        setModels(res2.data);
+        setModel(res2.data[0]);
+      });
     });
   };
 
-  // GETTING MY MODELS
+  // GETTING MY MODELS WHEN BRANDS CHANGE
   const getModels = e => {
     const modelsUrl = `https://ha.edu.uy/api/models?brand=${e.target.value}`;
     axios.get(modelsUrl).then(res => {
       setModels(res.data);
-      setModel(res.data[1]);
+      setModel(res.data[0]);
     });
   };
 
@@ -35,11 +45,11 @@ const Filter = ({ setCarsInfo, inDollars, setInDollars }) => {
     setInDollars(!inDollars);
   };
 
-  // HANDLE THE CURRENCY
+  // HANDLE THE FILTER
   const handleFilter = async () => {
     const filterUrl = `https://ha.edu.uy/api/cars?brand=${brand}&model=${model}`;
 
-    if (brand === '' || model === '') {
+    if (!brand === '' || model === '') {
       alert('You need to select a brand and a model');
     } else {
       const res = await fetch(filterUrl);
@@ -48,6 +58,7 @@ const Filter = ({ setCarsInfo, inDollars, setInDollars }) => {
     }
   };
 
+  // GET ALL THE CARS
   const getAllCars = async () => {
     const carsUrl = 'https://ha.edu.uy/api/cars';
     const res = await fetch(carsUrl);
@@ -56,54 +67,54 @@ const Filter = ({ setCarsInfo, inDollars, setInDollars }) => {
   };
 
   useEffect(() => {
-    getBrands();
+    getBrandsAndModelsOnLoad();
   }, []);
 
-  const initialValues = {};
-
   return (
-    <Formik initialValues={initialValues} onSubmit={() => {}}>
-      <Form>
-        {/* BRAND VALUES */}
-        <div className="form-group">
-          <Customlabel htmlFor="brand" label="Brands" />
-          <CustomSelect
-            onChange={e => {
-              getModels(e);
-              setBrand(e.target.value);
-            }}
-            name="brand"
-            options={brands}
-          />
-        </div>
-        {/* MODEL VALUES */}
-        <div className="form-group">
-          <Customlabel htmlFor="model" label="Models" />
-          <CustomSelect
-            onChange={e => setModel(e.target.value)}
-            name="model"
-            options={models}
-          />
-        </div>
-        {/* FILTER BUTTON */}
-        <Button type="submit" onClick={handleFilter} className="btn-block mb-3">
-          Filter
-        </Button>
-        {/* CURRENCY BUTTON */}
-        <Button
-          variant="secondary"
-          className="btn-block"
-          onClick={handleCurrency}
-        >
-          {inDollars ? 'Change To Pesos' : 'Change To Dollars'}
-        </Button>
+    <Form>
+      {/* BRAND VALUES */}
+      <div className="form-group">
+        <Customlabel htmlFor="brand" label="Brands" />
+        <CustomSelect
+          onChange={e => {
+            getModels(e);
+            setBrand(e.target.value);
+          }}
+          name="brand"
+          options={brands}
+        />
+      </div>
 
-        {/* FILTER BUTTON */}
-        <Button type="button" onClick={getAllCars} className="btn-block mb-3">
-          All cars
-        </Button>
-      </Form>
-    </Formik>
+      {/* MODEL VALUES */}
+      <div className="form-group">
+        <Customlabel htmlFor="model" label="Models" />
+        <CustomSelect
+          onChange={e => setModel(e.target.value)}
+          name="model"
+          options={models}
+        />
+      </div>
+
+      {/* FILTER BUTTON */}
+      <Button type="button" onClick={handleFilter} className="btn-block mb-3">
+        Filter
+      </Button>
+
+      {/* CURRENCY BUTTON */}
+      <Button
+        type="button"
+        variant="secondary"
+        className="btn-block"
+        onClick={handleCurrency}
+      >
+        {inDollars ? 'Change To Pesos' : 'Change To Dollars'}
+      </Button>
+
+      {/* FILTER BUTTON */}
+      <Button type="button" onClick={getAllCars} className="btn-block mb-3">
+        All cars
+      </Button>
+    </Form>
   );
 };
 
